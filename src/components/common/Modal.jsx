f1d90e813436efb0af021d5b10aed34808899223
Silas Abayomi <abayomisilas50@gmail.com>
2025-07-18 19:21:23 +0100

    modal on mobile

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -15,9 +15,9 @@ export default function Modal({ isOpen, onClose, logo, children }) {
 
   if (!isOpen) return null;
   return (
-    <div className="flex fixed inset-0 z-50 justify-center items-start bg-black bg-opacity-40 overflow-y-auto">
+    <div className="flex overflow-y-auto fixed inset-0 z-50 justify-center items-start bg-black bg-opacity-40">
       <div
-        className={`flex relative flex-col items-center p-4 sm:p-8 mt-16 sm:mt-32 w-full max-w-full sm:max-w-lg bg-white rounded-2xl shadow-lg transition-all duration-700 ease-out
+        className={`flex relative flex-col items-center p-4 sm:p-8 mt-32 sm:mt-32 w-full max-w-full sm:max-w-lg bg-white rounded-2xl shadow-lg transition-all duration-700 ease-out
           ${show ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-10'}`}
         style={{
           transitionProperty: 'opacity, transform',
@@ -26,7 +26,7 @@ export default function Modal({ isOpen, onClose, logo, children }) {
         {/* Close button */}
         <button
           onClick={onClose}
-          className="absolute top-2 right-2 sm:-top-2 sm:-right-2 text-2xl font-bold text-gray-400 bg-white rounded-full hover:text-gray-600 focus:outline-none z-10"
+          className="absolute top-2 right-2 z-10 text-2xl font-bold text-gray-400 bg-white rounded-full sm:-top-2 sm:-right-2 hover:text-gray-600 focus:outline-none"
           aria-label="Close modal"
         >
           <X className="size-5" />
